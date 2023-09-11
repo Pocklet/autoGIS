ced2472f25049779b4334bd931a095dc1a8609b9
Pocklet <silcockcaleb@gmail.com>
2023-09-11 16:57:27 +1200

    update

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -1,6 +1,18 @@
+import pandas as pd
+import numpy as np
 from shapely.geometry import Point
 from shapely.geometry import LineString
 from shapely.geometry import Polygon
+
+fp = r"C:\Users\calebs\Documents\pythonData\data/travel_times_2015_helsinki.txt"
+
+data = pd.read_csv(
+    fp,
+    sep = ";",
+    # delim_whitespace = True,
+    # usecols = ["from_x", "from_y", "to_x", "to_y", "total_route_time"],
+    # skiprows = [0],
+)
 
 # define function point
 def create_point_geometry(x, y):
@@ -75,6 +87,35 @@ print(lineLength)
 polygonLength = get_length(polygon1)
 print(polygonLength)
 
+print(data.head())
 
 
 
+# formating data columns
+# new_column_names = [
+#     "from_id","to_id","fromid_toid","route_number","at",
+#     "from_x","from_y","to_x","to_y","total_route_time",
+#     "route_time","route_distance","route_total_lines"
+#     ]
+
+# data.columns = new_column_names
+# print(data.head())
+
+subset = data[["from_x", "from_y", "to_x", "to_y", "total_route_time"]]
+print(subset.head())
+
+
+# formatting data
+print(subset.dtypes)
+
+# creating lists for origin and destination
+origin_points = []
+destination_points = []
+
+for i in range(len(subset)):
+    x = subset["from_x"], 
+    y = subset["from_y"],
+    z = create_point_geometry(x, y),
+    origin_points.append(z)
+
+print(origin_points)
